08a0e0ba0aa29500d30fc574fa9d24fddd44dc9a
jorgeduje <jorgeduje4@gmail.com>
2025-02-06 20:58:30 -0300

    se trabaja con hooks promesas y map

---
+++ src/components/common/counter/Counter.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const Counter = ({ darkMode }) => {
+  const [contador, setContador] = useState(0);
+  const [name, setName] = useState("pepe");
+  const cambiarNombre = () => {
+    setName("juan");
+  };
+
+  const sumar = () => {
+    setContador(contador + 1);
+  };
+  const restar = () => {
+    setContador(contador - 1);
+  };
+
+  // console.log("peticion fuera del effect");
+
+  useEffect(() => {
+    console.log("Una peticion a un servidor que esta en japón");
+  }, [name, darkMode]); // array de dependencias
+
+  return (
+    <div>
+      <button onClick={restar}>restar</button>
+      <h2>{contador}</h2>
+      <button onClick={sumar}>Sumar</button>
+      <button onClick={cambiarNombre}>Cambiar nombre</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,22 +1,31 @@
+import { useEffect, useState } from "react";
+import { products } from "../../../products";
 import { ProductCard } from "../../common/productCard/ProductCard";
 
-export const ItemListContainer = ({ greeting }) => {
-  let nombreDeUsuario = "pepe";
-  const saludar = () => {
-    console.log(nombreDeUsuario);
-  };
+export const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getProducts = new Promise((resolve, reject) => {
+      let isAdmin = true;
+      if (isAdmin) {
+        resolve(products);
+      } else {
+        reject({ message: "algo salio mal", status: 400 });
+      }
+    });
+
+    getProducts
+      .then((res) => setItems(res))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <section>
-      <h2> {greeting} </h2>
-
-      {console.log("hola")}
-
       <h2>Mis productos</h2>
-
-      <ProductCard title="titulo 1" price="precio 1" />
-      <ProductCard title="titulo 2" price="precio 2" />
-      <ProductCard title="titulo 3" price="precio 3" />
+      {items.map((item) => {
+        return <ProductCard key={item.id} item={item} />;
+      })}
     </section>
   );
 };
